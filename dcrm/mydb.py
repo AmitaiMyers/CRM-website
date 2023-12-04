70beda669f5d2a01e60dd6a04564e2a74142e653
AmitaiMyers <amitaimyers1@gmail.com>
2023-12-04 14:45:27 +0200

    Initaial Commit

---
+++ dcrm/mydb.py
@@ -0,0 +1,13 @@
+import mysql.connector
+
+dataBase = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='Amitai243896!'
+)
+
+# prepare a cursor object
+cursorObject = dataBase.cursor()
+# Create a database
+cursorObject.execute("CREATE DATABASE crmco")
+print("All done")
